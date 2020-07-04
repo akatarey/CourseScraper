1e23499cae12f4f168bffa0be750fa109affb93a
shrey <shreykshah@gmail.com>
2020-07-03 23:32:35 -0700

    scrape working

--- scraper.py
+++ scraper.py
@@ -1,5 +1,8 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+from bs4 import BeautifulSoup
 
 driver = webdriver.Firefox(executable_path="./geckodriver") #relative path
 url = "https://enroll.wisc.edu/search?term=1212" #local varibale is all caps
@@ -11,30 +14,26 @@ time.sleep(2)
 classes = set()
 
 prev = ""
-prev_counter = 0
+course = " "
 
+actions = ActionChains(driver)
+actions.send_keys(Keys.PAGE_DOWN)
 
-while prev_counter < 30:
-    fir = True
-    for course_path in driver.find_elements_by_xpath("/html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item"):
-        classes.add(course_path.find_element_by_xpath("//div/div[2]/div/div[1]/strong").get_attribute('innerHTML'))
-        if fir is True:
-            new = course_path.find_element_by_xpath("//div/div[2]/div/div[1]/strong").get_attribute('innerHTML')
-        fir = False
-    if new == prev:
-        prev_counter = prev_counter+1
-    else:
-        prev = new
-        prev_counter = 0
-print(classes)
-
-
-
-# /html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item[7]/div/div[2]/div/div[1]/strong
-# /html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item[4]/div/div[2]/div/div[1]/strong
-
-# /html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item[6]/div/div[2]/div/div[1]
-# /html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item[3]/div/div[2]/div/div[1]/strong
+fin = 1
+while prev != course:
+    prev = course
+    for course_path in driver.find_elements_by_xpath("/html/body/div[1]/div/div/div/div/md-card[2]/md-content/md-virtual-repeat-container/div/div[2]/md-list/md-list-item/div"):
+        try:
+            html = course_path.get_attribute('innerHTML')
+            soup = BeautifulSoup(html, "lxml")
+            course = soup.find('div', {"class":'result__name flex-80'}).strong.text.strip()
+            classes.add(course)
+        except:
+            pass
+    driver.find_element_by_xpath("/html/body/div[1]/div/div/div/div/md-card[2]").click()
+    actions.perform()
+    time.sleep(1)
+print(sorted(classes))
 
 # cw = csv.writer(open("classes.csv",'wb'))
 # cw.writerow(list(cols))
